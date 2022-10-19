ab86cbc47967e7fca627924a32b5d9d08caa468b
Mentol4ik <110991588+Mentol4ik@users.noreply.github.com>
2022-10-19 13:42:44 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/ru/mirea/task8/num12.java
@@ -0,0 +1,21 @@
+package ru.mirea.task8;
+
+public class num12 {
+    public static void recursion() {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        // Базовый случай
+        if (n > 0) {
+            // Шаг рекурсии / рекурсивное условие
+            if (n % 2 == 1) {
+                System.out.println(n);
+                recursion();
+            } else {
+                recursion();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        recursion(); // вызов рекурсивной функции
+    }
+}
